fd5700a0ad854b2f820d89d2433d87c43c9cd3e3
Thalisu <thalisondmcc@gmail.com>
2024-06-20 19:07:05 -0300

    feat: add mongoose conection

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,6 +2,23 @@ import express from "express";
 import cors from "cors";
 import { diagnosesRouter, patientsRouter } from "./routes";
 
+import mongoose from "mongoose";
+
+import logger from "./utils/logger";
+import config from "./utils/config";
+
+mongoose.set("strictQuery", false);
+logger.info("connecting to", config.MONGODB_URI);
+
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info("connected to MongoDB");
+  })
+  .catch((error) => {
+    logger.error("error connecting to MongoDB:", error.message);
+  });
+
 const app = express();
 
 app.use(express.json());
