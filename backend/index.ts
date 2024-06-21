71e39639c6eb3bb938b8a7545abcec2642405696
Thalisu <thalisondmcc@gmail.com>
2024-06-21 16:14:52 -0300

    fix: fix import

--- backend/index.ts
+++ backend/index.ts
@@ -1,12 +1,11 @@
 import app from "./src/app";
-import { info } from "./src/utils/logger";
+import config from "./src/utils/config";
+import logger from "./src/utils/logger";
 
 app.get("/api/ping", (_req, res) => {
   res.send("pong");
 });
 
-const PORT = 3003;
-
-app.listen(PORT, () => {
-  info(`Server running on port http://localhost:${PORT}`);
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port http://localhost:${config.PORT}`);
 });
