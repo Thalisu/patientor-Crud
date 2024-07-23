ac20e757937fc92881e6b5676c65e266bf356cfa
Thalisu <thalisondmcc@gmail.com>
2024-07-23 17:51:16 -0300

    feat: add healthRating of a patient to main page

--- frontend/src/components/HealthRatingBar.tsx
+++ frontend/src/components/HealthRatingBar.tsx
@@ -1,40 +1,14 @@
-import { Rating } from '@mui/material';
-import { Favorite } from '@mui/icons-material';
+import { Entry } from "../types";
 
-import { styled } from '@mui/material/styles';
+import { getHealth } from "../utils";
 
-type BarProps = {
-  rating: number;
-  showText: boolean;
-};
+const HealthRatingBar = ({ entries }: { entries?: Entry[] }) => {
+  const entry = entries?.reverse().find((e) => e.type === "HealthCheck");
+  const healthCheck = getHealth(entry?.healthCheckRating);
 
-const StyledRating = styled(Rating)({
-  iconFilled: {
-    color: "#ff6d75",
-  },
-  iconHover: {
-    color: "#ff3d47",
-  }
-});
-
-const HEALTHBAR_TEXTS = [
-  "The patient is in great shape",
-  "The patient has a low risk of getting sick",
-  "The patient has a high risk of getting sick",
-  "The patient has a diagnosed condition",
-];
-
-const HealthRatingBar = ({ rating, showText }: BarProps) => {
   return (
     <div className="health-bar">
-      <StyledRating
-        readOnly
-        value={4 - rating}
-        max={4}
-        icon={<Favorite fontSize="inherit" />}
-      />
-
-      {showText ? <p>{HEALTHBAR_TEXTS[rating]}</p> : null}
+      <p>{healthCheck}</p>
     </div>
   );
 };
